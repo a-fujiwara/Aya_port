7c3caf541ca9931e94ef6c851312d8a39e1c5fa5
早津渉 <top.men09@gmail.com>
2016-10-29 12:37:49 +0900

    jsファイルを変更しました

--- js/style.js
+++ js/style.js
@@ -1,12 +1,13 @@
 //cf(https://hayakawayuki.com/4518)
 $(function(){
   $('#main-nav li').on('click', function(){
-    if($(this).not('active')){
+  	$(this).children(".circle").addClass("circle_scaleUp is_active");
+   /* if($(this).not('active')){
       // nav
       $(this).addClass('active').siblings('li').removeClass('active');
       // content
       var index = $('#main-nav li').index(this);
       $('#contents div').eq(index).addClass('active').siblings('div').removeClass('active');
-    }
-  });
+    }*/
+  })
 });
